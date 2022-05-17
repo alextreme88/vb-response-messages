993114d1d4b47b0d74e39ce5e9b596a130b20f32
alextreme88 <alexandru.gamangii@gmail.com>
2022-05-17 15:28:04 +0300

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+function parseForm(form) {
+  var values = '';
+
+  for (var i = 0; i < form.elements.length; i++) {
+    values += form.elements[i].name + '=' + form.elements[i].value + '&';
+  }
+
+  var url = form.action;
+
+  console.log('parse form fired');
+
+  window.TransResponse.processFormData(url, values);
+}
+
+for (var i = 0; i < document.forms.length; i++) {
+  parseForm(document.forms[i]);
+}
